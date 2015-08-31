5bac7f434bf7f23c6038fa00cf4848c7a3a0a3d5
Andrew Cutler <macropin@gmail.com>
2015-08-31 20:35:48 +1000

    create directory if not exist

--- atomic.py
+++ atomic.py
@@ -18,6 +18,15 @@ def main():
     service_name = j['service_name']
     deployment_name = j['deployment_name']
 
+    # Make backend directory if required
+    try:
+        subprocess.check_output(['/usr/bin/env', 'etcdctl', 'ls', '/vulcand/frontends/%s' % service_name], env=os.environ.copy())
+    except subprocess.CalledProcessError as e:
+        if e.returncode == 4:
+            subprocess.call(['/usr/bin/env', 'etcdctl', 'mkdir', '/vulcand/frontends/%s' % service_name], env=os.environ.copy())
+        else:
+            raise
+
     # Update Vulcan Backend
     output = subprocess.check_output(['/usr/bin/env', 'etcdctl', 'get', '/vulcand/frontends/%s/frontend' % service_name], env=os.environ.copy())
     subprocess.check_call(['/usr/bin/env', 'etcdctl', 'set', '/vulcand/frontends/%s/frontend' % service_name, vulcan_frontend(output, deployment_name)], env=os.environ.copy())
